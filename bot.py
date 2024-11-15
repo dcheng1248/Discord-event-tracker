013cbbbcba0407134254809077c1eaf0f2dc2812
Jeff Carter <jeffrey.a.carter7.ctr@army.mil>
2024-11-15 14:04:54 -0600

    print error for troubleshooting

--- bot.py
+++ bot.py
@@ -504,6 +504,7 @@ async def on_command_error(ctx, error):
 	elif isinstance(error, commands.MissingRequiredArgument):
 		await ctx.send("An argument is missing in this command. Please use !help for command formatting.")
 	else:
+		print(error)
 		await ctx.send("An error occured with the command. Please contact the admins.")
 
 #help
